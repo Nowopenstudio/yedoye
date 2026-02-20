461d0a2326423e252ecff56e47852c473b8102b8
Ro Oeurn <ro@nowopen.studio>
2026-02-20 15:54:45 -0800

    new

--- app/recordings/single.tsx
+++ app/recordings/single.tsx
@@ -1,28 +1,32 @@
 'use client'
 
-import { animate, useMotionValue, motion } from "framer-motion";
 
+import { useEffect, useRef, useState } from "react";
 
-import useMeasure from "react-use-measure";
-import { useEffect, useState } from "react";
-import { Arrow, Logo} from "../svg/icons";
-import { getDateLong } from "../util/sanity";
 import Link from "next/link";
 import ReadScroll from "./readScroll";
 import Image from "next/image";
 import React from "react";
+import MuxPlayer from "@mux/mux-player-react";
 import { MuxVideo } from "../util/muxPlayer";
 
 
 
 export default function Single({data,count}:any) {
   const [active,setActive] = useState(false)
+  const playerRef = useRef<any>(null);
   
   const over=()=>{
     setActive(true);
+    if(playerRef.current){
+      playerRef.current.play();
+    }
   }
    const out=()=>{
     setActive(false);
+    if(playerRef.current){
+      playerRef.current.pause();
+    }
   }
   
   return (
@@ -34,21 +38,21 @@ export default function Single({data,count}:any) {
                               <Image alt="image" height={0}  width={0} sizes="100vw" src={data.cover.image}  className={`fadeOn w-full h-full object-cover`}/>
                             </div>
                           ):('')}
-                       {active?(
-                       <React.Fragment>
-                       {data.cover.image?(
-                            <div className="absolute top-0 left-0 w-full h-full z-[1]">
-                             <div className="h-full w-full bgMux noControl z-0"> <MuxVideo playbackId={data.cover.vid} title={`Home Video`} ratio={data.cover.ratio} play={true}/></div>
+                       
+                       {data.cover.vid?(
+                            <div className={`absolute top-0 left-0 w-full h-full z-[1] ${active?'opacity-100':'opacity-0'}`}>
+                             <div className="h-full w-full bgMux noControl z-0">
+                             <MuxPlayer key={`${data.cover.vid}-${'videoLayer'}`} ref={playerRef} poster={`https://image.mux.com/${data.cover.vid}/thumbnail.webp?time=0`} playbackId={data.cover.vid} playsInline autoPlay={false}  style={{ aspectRatio:`${data.cover.ratio.split(':')[0]}/${data.cover.ratio.split(':')[1]}`}} loop={true} metadata={{video_title:'homeVideo'}} />
+                              {/* <MuxVideo playbackId={data.cover.vid} title={`Home Video`} ratio={data.cover.ratio} play={true}/> */}
+                              </div>
                             </div>
                           ):('')}
                           
-                       </React.Fragment>
-                       ):('')}
+                    
                        <div className="bg-[--black] text-[--purple] w-full bottom-0 left-0 absolute z-10 overflow-hidden cta ">
                             <div className="bg-[--purple] w-full py-[--xs] text-[--black] text-center">
-                              <ReadScroll data={{'cta':`${data.info.date?`${data.info.date}  - `:''}${data.info.location?`${data.info.location} - `:''}${data.title}`,"url":`/recording/${data.slug}`}} time={14}/></div>
+                             <ReadScroll data={{'cta':`${data.info.date?`${data.info.date}  - `:''}${data.info.location?`${data.info.location} - `:''}${data.title}`,"url":`/recording/${data.slug}`}} time={14}/></div>
                             <ReadScroll data={{"cta":"View","url":`/recording/${data.slug}`}} time={4}/>
-                            
                           </div>
       
      </Link>
--- app/util/muxPlayer.tsx
+++ app/util/muxPlayer.tsx
@@ -17,10 +17,10 @@ export function MuxVideoBG({playbackId, title, ratio}: any) {
 
   if (!playbackId) return null
 
-  return <MuxPlayer ref={playerRef} poster={`https://image.mux.com/${playbackId}/thumbnail.webp?time=0`} playbackId={playbackId} metadata={title ? {video_title: title} : undefined} muted playsInline autoPlay={true} loop={true} style={{ aspectRatio:`${ratio.split(':')[0]}/${ratio.split(':')[1]}`}}/>
+  return <MuxPlayer key={`${playbackId}-${title}`} ref={playerRef} poster={`https://image.mux.com/${playbackId}/thumbnail.webp?time=0`} playbackId={playbackId} metadata={title ? {video_title: title} : undefined} muted playsInline autoPlay={true} loop={true} style={{ aspectRatio:`${ratio.split(':')[0]}/${ratio.split(':')[1]}`}}/>
 }
 
-export function MuxVideo({playbackId, title, poster, ratio,play}:any) {
+export function MuxVideo({playbackId, title, poster, ratio,play,auto}:any) {
   const playerRef = useRef<any>(null);
   useEffect(() => {
     // Cleanup function runs on dismount
@@ -33,5 +33,5 @@ export function MuxVideo({playbackId, title, poster, ratio,play}:any) {
   if (!playbackId) return null
 
   
-  return <MuxPlayer ref={playerRef} poster={poster?poster:`https://image.mux.com/${playbackId}/thumbnail.webp?time=0`} playbackId={playbackId} playsInline autoPlay={play} metadata={title ? {video_title: title} : undefined}  style={{ aspectRatio:`${ratio.split(':')[0]}/${ratio.split(':')[1]}`}} />
+  return <MuxPlayer key={`${playbackId}-${title}`} ref={playerRef} poster={poster?poster:`https://image.mux.com/${playbackId}/thumbnail.webp?time=0`} playbackId={playbackId} playsInline autoPlay={true} metadata={title ? {video_title: title} : undefined}  style={{ aspectRatio:`${ratio.split(':')[0]}/${ratio.split(':')[1]}`}} />
 }
