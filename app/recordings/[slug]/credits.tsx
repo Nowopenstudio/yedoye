390c7e283c37ac484c48d5243f8a6ce85b0b764a
Ro Oeurn <ro@nowopen.studio>
2026-02-23 23:37:55 -0800

    new

--- app/recordings/[slug]/credits.tsx
+++ app/recordings/[slug]/credits.tsx
@@ -10,8 +10,8 @@ export default function Credits({credits}:any) {
         credits.map((item:any,i:number)=>{
           return(
             <div className="inline-flex min-w-min items-start md:items-end fadeOn gap-2 " key={`credit-${i}`} style={{animationDelay:`${i*.25}s`}}>
-              <p className="whitespace-nowrap  uppercase footnote pt-0.5">{item.title}</p>
-              <p className="sm:whitespace-nowrap font-bold uppercase"><strong>{item.name}</strong></p>
+              <p className="whitespace-nowrap  uppercase footnote pt-1 sm:pt-2">{item.title}</p>
+              <p className="md:whitespace-nowrap font-bold uppercase"><strong>{item.name}</strong></p>
             </div>
           )
         })
